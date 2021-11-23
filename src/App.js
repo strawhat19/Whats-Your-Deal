914bbe57baddfacd5e6a69a5cd6e2a38b9a099ac
Rakib Ahmed <rakib987@gmail.com>
2021-11-23 02:24:42 -0500

    before Stock Bar Build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './sass/App.css';
 import Header from './components/Header/header';
+// import Header2 from './components/Header2/header2';
 import Main from './components/Main/main';
 import Footer from './components/Footer/footer';
 
 function App() {
   return (
     <div className="App">
-    <Header />
-    <Main />
-    <Footer />
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
