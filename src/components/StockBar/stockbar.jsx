97dc1ef0173317e5e1df60864a642272ccc94a76
Rakib Ahmed <rakib987@gmail.com>
2021-11-23 00:43:50 -0500

    update nav and footer with stockbar

--- src/components/StockBar/stockbar.jsx
+++ src/components/StockBar/stockbar.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import "./sass/stockbar.css";
 
 function Stockbar() {
-    return (
-        <div class="outerMarquee marqueeAnim">
-            <div class="newTesting" id="marqueeTest"></div>
-        </div>
-    );
+
+    function stockFetch() {
+        // Declare constants-- touch marquee element
+        const stockBar = document.getElementById(`stockBar`);
+
+        fetch(`https://financialmodelingprep.com/api/v3/available-traded/list?apikey=7e60778244bbb11a3e59192e565ed625`)
+        .then(response => {
+            return response.json();
+        }).then(stockList => {
+            console.log('Stock API Data Is:');
+            stockList.splice(100);
+            console.log(stockList);
+        })
+    }
+    
+    // stockFetch();
+
+    return <div class="stockBar" id="stockBar"></div>;
 }
  
 export default Stockbar;
