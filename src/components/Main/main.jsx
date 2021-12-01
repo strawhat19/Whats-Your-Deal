d517dbc4f13d44a16d235f920200c7800d39583f
Rakib Ahmed <rakib987@gmail.com>
2021-11-30 19:42:49 -0500

    turn off fetching for now

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -3,13 +3,14 @@ import "./sass/main.css";
 import "./css/customCSS.css";
 import Company from './states/Company/company';
 import Homepage from './states/Homepage/homepage';
+import Login from '../Login/login'
 
 export default class Main extends React.Component {
     render(state) {
         state = this.props.state;
         return (
             <main class="main">
-                <Company />
+                <Login />
             </main>
         );
     }
